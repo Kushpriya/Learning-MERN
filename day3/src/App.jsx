8be129176237e1b1e3540e0977dee93fcc228bb4
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-06 17:10:18 +0545

    day3

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Card from "./components/Card";
+import Image1 from "../src/assets/image/img1.jpg";
+import Image2 from "../src/assets/image/img2.jpg";
+import Image3 from "../src/assets/image/img3.jpg";
+import Image4 from "../src/assets/image/img4.jpg";
+import Image5 from "../src/assets/image/img5.jpg";
 
 class App extends React.Component{
   render(){
     return (<div>
-      <Card name="zdxgfchvjh"/>
-      <Card name="zdxgf"/>
-      <Card name="zdxgf"/>
-      <Card name="zdxgf"/>
-      <Card name="zdxgf"/>
-      <Card name="zdxgf"/>
+    
+      <Card  image ={Image1} name="Byron Martinez" position="Senior Software Enginer"/>
+      <Card  image ={Image2} name="Lena Knight" position="Associate Software Enginer"/>
+      <Card  image ={Image3} name="Sandra Smith" position="Project Manager"/>
+      <Card  image ={Image4} name="Dwight Harper" position="Business Analyst"/>
+      <Card  image ={Image5} name="Dylan Jensen" position="Junior Software Enginer"/>
+      
     </div>
     )
   }
 }
-export default App
+export default App;
--- day3/src/components/Card.jsx
+++ day3/src/components/Card.jsx
@@ -4,7 +4,12 @@ class Card extends React.Component{
     render(){
         return(
             <div className="main">
-                <h1>{this.props.name}</h1>
+                <img src={this.props.image} className="img"/>
+               <div>
+                    <h1 className="name">{this.props.name}</h1>
+                    <p className="para">{this.props.position}</p>
+                </div> 
+                <button className="btn">Invite</button>
             </div>
         )
     }
