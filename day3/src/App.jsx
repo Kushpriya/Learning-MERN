69fc58644ba5621d5e6971941590cbf489a45277
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-06 19:29:05 +0545

    user list array

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -8,16 +8,22 @@ import Image5 from "../src/assets/image/img5.jpg";
 
 class App extends React.Component{
   render(){
-    return (<div>
-    
-      <Card  image ={Image1} name="Byron Martinez" position="Senior Software Enginer"/>
-      <Card  image ={Image2} name="Lena Knight" position="Associate Software Enginer"/>
-      <Card  image ={Image3} name="Sandra Smith" position="Project Manager"/>
-      <Card  image ={Image4} name="Dwight Harper" position="Business Analyst"/>
-      <Card  image ={Image5} name="Dylan Jensen" position="Junior Software Enginer"/>
-      
+    const arr = [{image:Image1, name:"Byron Martinez", position:"Senior Software Enginer"},
+      {image:Image2 , name:"Lena Knight", position:"Associate Software Enginer"},
+      {image:Image3 , name:"Sandra Smith" ,position:"Project Manager"},
+      {image:Image4 , name:"Dwight Harper",position:"Business Analyst"},
+      {image:Image5 , name:"Dylan Jensen", position:"Junior Software Enginer"}
+    ];
+    return (
+      <div>
+        {
+          arr.map((data) => (
+            <Card  image={data.image} name={data.name} position={data.position}/>
+          )
+        )
+        }
     </div>
-    )
+    );
   }
 }
 export default App;
