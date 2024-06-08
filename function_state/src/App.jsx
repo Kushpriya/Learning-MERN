db217887da6a1bd40dd56b42fe3fda7a9b831cf7
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-08 16:05:12 +0545

    toggle added

--- function_state/src/App.jsx
+++ function_state/src/App.jsx
@@ -18,27 +18,48 @@
 // export default App;
 
 
-// STATE
-import {useState} from "react";
+// // STATE
+// import {useState} from "react";
 
-function App(){
-    const[data,setData] =useState("Priya");
-    // const [data,setData] = useState(0);
+// function App(){
+//     const[data,setData] =useState("Priya");
+//     // const [data,setData] = useState(0);
 
-  function Data(){
-    setData("Kushawaha");
-    // setData(data+1);
+//   function Data(){
+//     setData("Kushawaha");
+//     // setData(data+1);
 
-  }
+//   }
 
-  return(
-    <div>
-        <h1>{data}</h1>
-        <button onClick={Data}> click!!!</button>
-    </div>
+//   return(
+//     <div>
+//         <h1>{data}</h1>
+//         <button onClick={Data}> click!!!</button>
+//     </div>
         
-  );
+//   );
 
-}
+// }
 
-export default App;
+// export default App;
+
+
+//Toggle
+import React from "react";
+
+
+ function App(){
+   const [status,setStatus] = React.useState(false);
+   return(
+    <div className="App">{
+     status? <h1>Priya Kushawaha</h1>:null
+
+    }
+      {/* <button onClick={() =>setStatus(false)}>Hide</button>
+      <button onClick={() =>setStatus(true)}>Show</button> */}
+      <button onClick={() =>setStatus(!status)}>Toggle</button>
+    </div>
+   );
+ }
+
+ export default App;
